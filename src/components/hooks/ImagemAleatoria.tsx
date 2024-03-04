0c6a26e1b39b05289fcaa303ebcb0b8bc6e54925
shenrique1970 <shvribeiro@hotmail.com>
2024-03-04 18:28:28 -0300

    atualização

--- src/components/hooks/ImagemAleatoria.tsx
+++ src/components/hooks/ImagemAleatoria.tsx
@@ -1,7 +1,31 @@
 export default function ImagemAleatoria() {
+
+    let pesquisa: string = ''
+
+    const url = 'https://source.unsplash.com/featured/300x300?'
+
+    function urlImagem() {
+        return `${url}${pesquisa}`
+    }
+
+    function remderizarBotao(valor: string) {
+        return (
+            <button className={`
+                bg-blue-500 px-4 py-2 rounded-md p-2
+            `} onClick={() => {
+                    pesquisa = valor
+                    console.log(urlImagem())
+                }}>
+                {valor}
+            </button>
+        )
+    }
+
     return (
-        <div>
-            imagemAleatoria
+        <div className="flex flex-col gap-3">
+            {remderizarBotao('abstract')}
+            {remderizarBotao('city')}
+            {remderizarBotao('person')}
         </div>
     )
 }
