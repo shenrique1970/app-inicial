8ae7d733b86bf192d97816386ccf508877e07dec
shenrique1970 <shvribeiro@hotmail.com>
2024-03-06 07:25:56 -0300

    atualização

--- src/components/hooks/ImagemAleatoria.tsx
+++ src/components/hooks/ImagemAleatoria.tsx
@@ -1,20 +1,23 @@
+import Image from "next/image"
+import { useState } from "react"
+
 export default function ImagemAleatoria() {
 
-    let pesquisa: string = ''
+    const [pesquisa, alterarPesquisa] = useState<string>('abstract')
+    const [tamanho, alterarTanho] = useState<number>(300)
 
-    const url = 'https://source.unsplash.com/featured/300x300?'
+    const url = 'https://source.unsplash.com/featured/'
 
-    function urlImagem() {
-        return `${url}${pesquisa}`
-    }
-
+   
     function remderizarBotao(valor: string) {
         return (
             <button className={`
-                bg-blue-500 px-4 py-2 rounded-md p-2
+                bg-blue-500
+                px-4 py-2 
+                rounded-md p-2
             `} onClick={() => {
-                    pesquisa = valor
-                    console.log(urlImagem())
+                alterarPesquisa(valor)
+                    console.log(valor)
                 }}>
                 {valor}
             </button>
@@ -22,10 +25,27 @@ export default function ImagemAleatoria() {
     }
 
     return (
-        <div className="flex flex-col gap-3">
-            {remderizarBotao('abstract')}
-            {remderizarBotao('city')}
-            {remderizarBotao('person')}
+        <div className="flex flex-col gap-3 border border-zinc-700 p-7 rounded-md">
+            <div className="flex justify-center gap-7">
+                <span>{pesquisa}</span>
+                <span>{tamanho}x{tamanho}</span>
+            </div>
+            <Image src={`${url}${tamanho}x${tamanho}?${pesquisa}`} width={300} height={300} alt="Imagem" />
+            <div className="flex justify-between">
+                {remderizarBotao('abstract')}
+                {remderizarBotao('city')}
+                {remderizarBotao('person')}
+            </div>
+            <div>
+                <input 
+                type="number" 
+                value={tamanho} 
+                className="bg-zinc-800 p-2 rounded-md outline-none text-white"
+                onChange={e => {
+                    alterarTanho(+e.target.value)
+                }}
+                />
+            </div>
         </div>
     )
 }
